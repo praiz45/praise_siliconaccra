e2ef95c77372ee144971b6d508bfb2cb6a6b08e3
praiz45 <146184799+praiz45@users.noreply.github.com>
2023-10-25 12:16:09 +0000

    Add files via upload
    done

---
+++ program.py
@@ -0,0 +1,33 @@
+#measurement = ["233","390","450","690"]
+
+#new_measure = [int(m) / 10 for m in measurement]
+
+#print(new_measure)
+
+#series = [406,789,-9909,456,224]
+#new_series = [serie /10 for serie in series if serie !=-9909]
+
+#print(new_series)
+#new_series1 = [serie /10 if serie !=-9909 else 2 for serie in series]
+#print(new_series1)
+
+
+
+#def car1(slef,engine,speed,torque,horse_power,transmission,body):
+   #slef.engine = engine
+   #slef.speed = speed
+  # slef.torque = torque
+   #slef.horse_power = horse_power
+   #slef.transmission = transmission
+   #slef.body = body
+
+#print("m"+car1)
+
+#car1("mercedes")
+
+
+
+def triangle(a,b):
+   return 1/2 * (a*b)**2
+print(triangle(5,5))
+   
